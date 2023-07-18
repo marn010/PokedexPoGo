b2d5539a60d74f3752199ffca2e3db707371187f
Miguel Rivera <mariveran@unal.edu.co>
2023-07-18 14:12:11 -0500

    change pic name, implements a map to print db info

--- src/components/Pokedex/main.js
+++ src/components/Pokedex/main.js
@@ -2,30 +2,33 @@ import "./pokedex.css";
 
 import { Link } from "react-router-dom";
 
-import Pokemon from "./Pokemon/main";
-
-import db from '../db/db.json';
+import db from "../db/db.json";
 
 export default function pokedex() {
   /* console.log("Try: " + db.pokes[0].id) */
   return (
     <div className="Pokedex">
       <h2>Pokedex Page!</h2>
-      <div className="Pokemon">
-        <div className="PokemonPic">
-          <Link to="/Pokemon">
-            <img src="/assets/001.png" alt="Pokemon" />
-          </Link>
-        </div>
-        <div className="PokemonInfo">
-          
-          <p># {db.pokes[0].id}{db.pokes[0].Name}</p>
-        </div>
-        <div>
-          <p>Tipos</p>
-        </div>
-      </div>
-      {/* <Pokemon/> */}
+      {db.pokes.map((item,id) => {
+        return (
+          <div className="Pokemon">
+            <div className="PokemonPic">
+              <Link to="/Pokemon">
+                <img src={item.PokePic} alt={item.Name} />
+              </Link>
+            </div>
+            <div className="PokemonInfo">
+              <p>
+                # {db.pokes[id].id} 
+                {db.pokes[id].Name}
+              </p>
+            </div>
+            <div>
+              <p>Tipos</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
