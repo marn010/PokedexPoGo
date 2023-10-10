30d293fad91901f78fff6e066e5a83195a30b55a
Miguel Rivera <mariveran@unal.edu.co>
2023-10-09 20:31:13 -0500

    Fix visual issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Footer from "./components/Footer/Footer";
-import Main from "./components/Main/Main";
+//import Main from "./components/Main/Main";
 import { Routes, Route, } from "react-router-dom";
 
 import Pokedex from './components/Pokedex/main';
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Pokedex />} />
         <Route path="/Pokedex" element={<Pokedex/>} />
         <Route path="/PvP" element={<PvP/>} />
         <Route path="/Pokemon" element={<Pokemon/>} />
